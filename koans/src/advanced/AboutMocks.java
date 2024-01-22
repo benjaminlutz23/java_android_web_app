a728332a915c44a092f705a5a6535598587be301
Benjamin Lutz <benlutz@cecs.pdx.edu>
2024-01-21 21:30:17 -0800

    advanced done

--- koans/src/advanced/AboutMocks.java
+++ koans/src/advanced/AboutMocks.java
@@ -37,10 +37,12 @@ public class AboutMocks {
 
     @Koan
     public void simpleAnonymousMock() {
-        // HINT: pass a safe Collaborator implementation to constructor
-        // new ClassUnderTest(new Collaborator(){... it should not be the
-        // objective of this test to test that collaborator, so replace it
-        new ClassUnderTest().doSomething();
+        Collaborator safeCollaborator = new Collaborator() {
+            public void doBusinessStuff(){
+            }
+        };
+
+        new ClassUnderTest(safeCollaborator).doSomething();
     }
 
 }
