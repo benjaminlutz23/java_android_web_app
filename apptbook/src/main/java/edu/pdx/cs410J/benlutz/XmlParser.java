4ab56af090f91379eee486b6224d7d299b522b97
benjaminlutz <benlutz@pdx.edu>
2024-02-27 19:01:35 -0800

    XmlParser and tests are looking pretty good

--- apptbook/src/main/java/edu/pdx/cs410J/benlutz/XmlParser.java
+++ apptbook/src/main/java/edu/pdx/cs410J/benlutz/XmlParser.java
@@ -1,21 +1,64 @@
 package edu.pdx.cs410J.benlutz;
 
-import edu.pdx.cs410J.AbstractAppointmentBook;
 import edu.pdx.cs410J.AppointmentBookParser;
 import edu.pdx.cs410J.ParserException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.InputStream;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
-public class XmlParser implements AppointmentBookParser {
+public class XmlParser implements AppointmentBookParser<AppointmentBook> {
     private final InputStream xml;
-
 
     public XmlParser(InputStream xml) {
         this.xml = xml;
     }
 
     @Override
-    public AbstractAppointmentBook parse() throws ParserException {
-        return null;
+    public AppointmentBook parse() throws ParserException {
+        try {
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(xml);
+            doc.getDocumentElement().normalize();
+
+            String owner = doc.getDocumentElement().getElementsByTagName("owner").item(0).getTextContent();
+            AppointmentBook book = new AppointmentBook(owner);
+
+            NodeList nList = doc.getElementsByTagName("appointment");
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy hh:mm a");
+            ZoneId zoneId = ZoneId.systemDefault(); // Use the system's default timezone or specify one like ZoneId.of("America/New_York")
+
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                Node nNode = nList.item(temp);
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    String description = eElement.getElementsByTagName("description").item(0).getTextContent();
+                    String beginTimeString = eElement.getElementsByTagName("beginTime").item(0).getTextContent();
+                    String endTimeString = eElement.getElementsByTagName("endTime").item(0).getTextContent();
+
+                    LocalDateTime beginLocalDateTime = LocalDateTime.parse(beginTimeString, formatter);
+                    LocalDateTime endLocalDateTime = LocalDateTime.parse(endTimeString, formatter);
+                    ZonedDateTime beginTime = ZonedDateTime.of(beginLocalDateTime, zoneId);
+                    ZonedDateTime endTime = ZonedDateTime.of(endLocalDateTime, zoneId);
+
+                    Appointment appointment = new Appointment(description, beginTime, endTime);
+                    book.addAppointment(appointment);
+                }
+            }
+            return book;
+        } catch (Exception | invalidDescriptionException e) {
+            throw new ParserException("Error parsing XML", e);
+        } catch (invalidOwnerException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- apptbook/src/test/java/edu/pdx/cs410J/benlutz/XmlParserTest.java
+++ apptbook/src/test/java/edu/pdx/cs410J/benlutz/XmlParserTest.java
@@ -1,4 +1,67 @@
 package edu.pdx.cs410J.benlutz;
 
+import edu.pdx.cs410J.ParserException;
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class XmlParserTest {
+
+    @Test
+    public void testParseEmptyAppointmentBook() throws ParserException {
+        String xml = "<appointmentBook><owner>Owner Name</owner></appointmentBook>";
+        ByteArrayInputStream xmlStream = new ByteArrayInputStream(xml.getBytes());
+        XmlParser parser = new XmlParser(xmlStream);
+
+        AppointmentBook book = parser.parse();
+        assertEquals("Owner Name", book.getOwnerName(), "Owner name should match");
+        assertTrue(book.getAppointments().isEmpty(), "Appointment book should be empty");
+    }
+
+    @Test
+    public void testParseSingleAppointment() throws ParserException {
+        String xml = "<appointmentBook><owner>Owner Name</owner>"
+                + "<appointment><description>Test Appointment</description>"
+                + "<beginTime>01/01/2024 10:00 AM</beginTime>"
+                + "<endTime>01/01/2024 11:00 AM</endTime></appointment></appointmentBook>";
+        ByteArrayInputStream xmlStream = new ByteArrayInputStream(xml.getBytes());
+        XmlParser parser = new XmlParser(xmlStream);
+
+        AppointmentBook book = parser.parse();
+        assertEquals(1, book.getAppointments().size(), "Appointment book should have one appointment");
+
+        Appointment appointment = book.getAppointments().iterator().next();
+        assertEquals("Test Appointment", appointment.getDescription(), "Appointment description should match");
+    }
+
+    @Test
+    public void testParseMultipleAppointments() throws ParserException {
+        String xml = "<appointmentBook><owner>Owner Name</owner>"
+                + "<appointment><description>First Appointment</description>"
+                + "<beginTime>01/01/2024 10:00 AM</beginTime>"
+                + "<endTime>01/01/2024 11:00 AM</endTime></appointment>"
+                + "<appointment><description>Second Appointment</description>"
+                + "<beginTime>01/02/2024 10:00 AM</beginTime>"
+                + "<endTime>01/02/2024 11:00 AM</endTime></appointment></appointmentBook>";
+        ByteArrayInputStream xmlStream = new ByteArrayInputStream(xml.getBytes());
+        XmlParser parser = new XmlParser(xmlStream);
+
+        AppointmentBook book = parser.parse();
+        assertEquals(2, book.getAppointments().size(), "Appointment book should have two appointments");
+    }
+
+    @Test
+    public void parseInvalidXmlFormat() {
+        String invalidXml = "<appointmentBook><owner>Owner Name</owner>"
+                + "<appointment><description>Test Appointment</description>"
+                + "<beginTime>01/01/2024 10:00 AM</beginTime>" // Missing endTime tag
+                + "</appointment></appointmentBook>";
+
+        InputStream xmlStream = new ByteArrayInputStream(invalidXml.getBytes());
+        XmlParser parser = new XmlParser(xmlStream);
+
+        assertThrows(ParserException.class, () -> parser.parse());
+    }
 }
