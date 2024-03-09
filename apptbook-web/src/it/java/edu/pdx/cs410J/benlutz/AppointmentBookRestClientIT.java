182740b9172c75acd7823152fc70a7809fd45c38
benjaminlutz <benlutz@pdx.edu>
2024-03-09 11:24:00 -0800

    I'm about to redo this

--- apptbook-web/src/it/java/edu/pdx/cs410J/benlutz/AppointmentBookRestClientIT.java
+++ apptbook-web/src/it/java/edu/pdx/cs410J/benlutz/AppointmentBookRestClientIT.java
@@ -1,66 +1,66 @@
-package edu.pdx.cs410J.benlutz;
-
-import org.junit.jupiter.api.MethodOrderer.MethodName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
-
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
-/**
- * Integration test that tests the REST calls made by {@link AppointmentBookRestClient}
- */
-@TestMethodOrder(MethodName.class)
-class AppointmentBookRestClientIT {
-  private static final String HOSTNAME = "localhost";
-  private static final String PORT = System.getProperty("http.port", "8080");
-
-  private AppointmentBookRestClient newAppointmentBookRestClient() {
-    int port = Integer.parseInt(PORT);
-    return new AppointmentBookRestClient(HOSTNAME, port);
-  }
-
-//  @Test
-//  void test0RemoveAllAppointmentBooks() throws IOException {
-//    AppointmentBookRestClient client = newAppointmentBookRestClient();
-//    client.removeAllAppointmentBooks();
+//package edu.pdx.cs410J.benlutz;
+//
+//import org.junit.jupiter.api.MethodOrderer.MethodName;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.TestMethodOrder;
+//
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//
+//import static org.hamcrest.MatcherAssert.assertThat;
+//import static org.hamcrest.Matchers.containsString;
+//import static org.junit.jupiter.api.Assertions.assertThrows;
+//
+///**
+// * Integration test that tests the REST calls made by {@link AppointmentBookRestClient}
+// */
+//@TestMethodOrder(MethodName.class)
+//class AppointmentBookRestClientIT {
+//  private static final String HOSTNAME = "localhost";
+//  private static final String PORT = System.getProperty("http.port", "8080");
+//
+//  private AppointmentBookRestClient newAppointmentBookRestClient() {
+//    int port = Integer.parseInt(PORT);
+//    return new AppointmentBookRestClient(HOSTNAME, port);
 //  }
-
-  @Test
-  void test1EmptyServerContainsNoAppointmentBooks() {
-    AppointmentBookRestClient client = newAppointmentBookRestClient();
-    String owner = "OwnerWithNoAppointments";
-    Exception exception = assertThrows(IOException.class, () -> client.searchAppointments(owner, "", ""));
-    assertThat(exception.getMessage(), containsString(String.valueOf(HttpServletResponse.SC_NOT_FOUND)));
-  }
-
-  @Test
-  void test2CreateNewAppointment() throws IOException {
-    AppointmentBookRestClient client = newAppointmentBookRestClient();
-    String owner = "Test Owner";
-    String description = "Test Description";
-    String begin = "1/1/2020 10:00 AM";
-    String end = "1/1/2020 11:00 AM";
-    client.addAppointment(owner, description, begin, end);
-
-    String appointments = client.searchAppointments(owner, begin, end);
-    assertThat(appointments, containsString(description));
-  }
-
-  @Test
-  void test4AddAppointmentToNonExistentOwnerCreatesAppointmentBook() throws IOException {
-    AppointmentBookRestClient client = newAppointmentBookRestClient();
-    String owner = "NewOwner";
-    String description = "New Description";
-    String begin = "1/2/2020 10:00 AM";
-    String end = "1/2/2020 11:00 AM";
-    client.addAppointment(owner, description, begin, end);
-
-    String result = client.searchAppointments(owner, begin, end);
-    assertThat(result, containsString(description));
-  }
-}
+//
+////  @Test
+////  void test0RemoveAllAppointmentBooks() throws IOException {
+////    AppointmentBookRestClient client = newAppointmentBookRestClient();
+////    client.removeAllAppointmentBooks();
+////  }
+//
+//  @Test
+//  void test1EmptyServerContainsNoAppointmentBooks() {
+//    AppointmentBookRestClient client = newAppointmentBookRestClient();
+//    String owner = "OwnerWithNoAppointments";
+//    Exception exception = assertThrows(IOException.class, () -> client.searchAppointments(owner, "", ""));
+//    assertThat(exception.getMessage(), containsString(String.valueOf(HttpServletResponse.SC_NOT_FOUND)));
+//  }
+//
+//  @Test
+//  void test2CreateNewAppointment() throws IOException {
+//    AppointmentBookRestClient client = newAppointmentBookRestClient();
+//    String owner = "Test Owner";
+//    String description = "Test Description";
+//    String begin = "1/1/2020 10:00 AM";
+//    String end = "1/1/2020 11:00 AM";
+//    client.addAppointment(owner, description, begin, end);
+//
+//    String appointments = client.searchAppointments(owner, begin, end);
+//    assertThat(appointments, containsString(description));
+//  }
+//
+//  @Test
+//  void test4AddAppointmentToNonExistentOwnerCreatesAppointmentBook() throws IOException {
+//    AppointmentBookRestClient client = newAppointmentBookRestClient();
+//    String owner = "NewOwner";
+//    String description = "New Description";
+//    String begin = "1/2/2020 10:00 AM";
+//    String end = "1/2/2020 11:00 AM";
+//    client.addAppointment(owner, description, begin, end);
+//
+//    String result = client.searchAppointments(owner, begin, end);
+//    assertThat(result, containsString(description));
+//  }
+//}
--- apptbook-web/src/test/java/edu/pdx/cs410J/benlutz/TextDumperParserTest.java
+++ apptbook-web/src/test/java/edu/pdx/cs410J/benlutz/TextDumperParserTest.java
@@ -1,40 +1,76 @@
-//package edu.pdx.cs410J.benlutz;
-//
-//import edu.pdx.cs410J.ParserException;
-//import org.junit.jupiter.api.Test;
-//
-//import java.io.StringReader;
-//import java.io.StringWriter;
-//import java.util.Collections;
-//import java.util.Map;
-//
-//import static org.hamcrest.MatcherAssert.assertThat;
-//import static org.hamcrest.Matchers.equalTo;
-//
-//public class TextDumperParserTest {
-//
-//  @Test
-//  void emptyMapCanBeDumpedAndParsed() throws ParserException {
-//    Map<String, String> map = Collections.emptyMap();
-//    Map<String, String> read = dumpAndParse(map);
-//    assertThat(read, equalTo(map));
-//  }
-//
-//  private Map<String, String> dumpAndParse(Map<String, String> map) throws ParserException {
-//    StringWriter sw = new StringWriter();
-//    TextDumper dumper = new TextDumper(sw);
-//    dumper.dump(map);
-//
-//    String text = sw.toString();
-//
-//    TextParser parser = new TextParser(new StringReader(text));
-//    return parser.parse();
-//  }
-//
-//  @Test
-//  void dumpedTextCanBeParsed() throws ParserException {
-//    Map<String, String> map = Map.of("one", "1", "two", "2");
-//    Map<String, String> read = dumpAndParse(map);
-//    assertThat(read, equalTo(map));
-//  }
-//}
+package edu.pdx.cs410J.benlutz;
+
+import edu.pdx.cs410J.ParserException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.*;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+
+public class TextDumperTest {
+
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("M/d/yyyy h:mm a VV");
+
+
+    @Test
+    void appointmentBookOwnerIsDumpedInTextFormat() throws invalidOwnerException {
+        // Create an appointment book with a specific owner
+        String owner = "Test Appointment Book";
+        AppointmentBook book = new AppointmentBook(owner);
+
+        // Dump the appointment book to text format
+        StringWriter sw = new StringWriter();
+        TextDumper dumper = new TextDumper(sw);
+        dumper.dump(book);
+
+        // Verify that the owner's name is correctly dumped
+        String text = sw.toString();
+        assertThat(text, containsString(owner));
+    }
+
+    @Test
+    void appointmentDetailsAreDumpedInTextFormat() throws invalidOwnerException, invalidDescriptionException {
+        // Create an appointment with specific details
+        ZonedDateTime beginTime = ZonedDateTime.now();
+        ZonedDateTime endTime = beginTime.plusHours(1);
+        Appointment appointment = new Appointment("Test Description", beginTime, endTime);
+        AppointmentBook book = new AppointmentBook("Test Appointment Book");
+        book.addAppointment(appointment);
+
+        // Dump the appointment book to text format
+        StringWriter sw = new StringWriter();
+        TextDumper dumper = new TextDumper(sw);
+        dumper.dump(book);
+
+        // Verify that the appointment details are correctly dumped
+        String text = sw.toString();
+        String expectedAppointmentString = String.format("%s, %s, %s",
+                appointment.getDescription(),
+                DATE_TIME_FORMATTER.format(beginTime),
+                DATE_TIME_FORMATTER.format(endTime));
+        assertThat(text, containsString(expectedAppointmentString));
+    }
+
+
+
+    // Do this one after writing the text parser class
+    @Test
+    void canParseTextWrittenByTextDumper(@TempDir File tempDir) throws IOException, ParserException, invalidOwnerException {
+        String owner = "Test Appointment Book";
+        AppointmentBook book = new AppointmentBook(owner);
+
+        File textFile = new File(tempDir, "apptbook.txt");
+        TextDumper dumper = new TextDumper(new FileWriter(textFile));
+        dumper.dump(book);
+
+        TextParser parser = new TextParser(new FileReader(textFile));
+        AppointmentBook read = (AppointmentBook) parser.parse();
+        assertThat(read.getOwnerName(), equalTo(owner));
+    }
+
+}
