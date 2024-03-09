a870fb8389620d80e9560e2ac5337f0f6a299c1d
benjaminlutz <benlutz@pdx.edu>
2024-03-09 10:54:57 -0800

    added exception classes

---
+++ apptbook-web/src/main/java/edu/pdx/cs410J/benlutz/invalidOwnerException.java
@@ -0,0 +1,8 @@
+package edu.pdx.cs410J.benlutz;
+
+/**
+ * Exception thrown when an invalid owner name is encountered.
+ * This exception is used to indicate that the owner name provided for an appointment book is invalid.
+ */
+public class invalidOwnerException extends Throwable {
+}
