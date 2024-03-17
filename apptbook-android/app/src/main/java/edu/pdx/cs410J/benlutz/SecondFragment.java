2c80b669a9427f53ecc3023695fc0466c81cd258
benjaminlutz <benlutz@pdx.edu>
2024-03-17 12:56:36 -0700

    Got an okay UI marked up for entering appointment book details

--- apptbook-android/app/src/main/java/edu/pdx/cs410J/benlutz/SecondFragment.java
+++ apptbook-android/app/src/main/java/edu/pdx/cs410J/benlutz/SecondFragment.java
@@ -1,13 +1,25 @@
 package edu.pdx.cs410J.benlutz;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.TimeZone;
 
 import edu.pdx.cs410J.benlutz.databinding.FragmentSecondBinding;
 
@@ -16,23 +28,49 @@ public class SecondFragment extends Fragment {
     private FragmentSecondBinding binding;
 
     @Override
-    public View onCreateView(
-            @NonNull LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
-
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
         binding = FragmentSecondBinding.inflate(inflater, container, false);
         return binding.getRoot();
-
     }
 
+    @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonSecond.setOnClickListener(v ->
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment)
-        );
+        binding.backButton.setOnClickListener(v ->
+                NavHostFragment.findNavController(SecondFragment.this).popBackStack());
+
+        // Time Zone Spinner setup
+        setupTimeZoneSpinner();
+
+        // Date and Time pickers for start and end time
+        setupDateTimePicker(binding.startTime);
+        setupDateTimePicker(binding.endTime);
+    }
+
+    private void setupTimeZoneSpinner() {
+        Spinner timeZoneSpinner = binding.timeZoneSpinner;
+        String[] timeZones = TimeZone.getAvailableIDs();
+        Arrays.sort(timeZones); // Sort time zones alphabetically
+        ArrayAdapter<String> timeZoneAdapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_item, timeZones);
+        timeZoneAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        timeZoneSpinner.setAdapter(timeZoneAdapter);
+    }
+
+    private void setupDateTimePicker(EditText editText) {
+        editText.setOnClickListener(v -> {
+            final Calendar calendar = Calendar.getInstance();
+            DatePickerDialog datePickerDialog = new DatePickerDialog(requireContext(), (view, year, month, dayOfMonth) -> {
+                TimePickerDialog timePickerDialog = new TimePickerDialog(requireContext(), (view1, hourOfDay, minute) -> {
+                    ZonedDateTime zonedDateTime = ZonedDateTime.of(year, month + 1, dayOfMonth, hourOfDay, minute, 0, 0, ZoneId.systemDefault());
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+                    editText.setText(zonedDateTime.format(formatter));
+                }, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), true);
+                timePickerDialog.show();
+            }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+            datePickerDialog.show();
+        });
     }
 
     @Override
@@ -40,5 +78,4 @@ public class SecondFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
-
 }
