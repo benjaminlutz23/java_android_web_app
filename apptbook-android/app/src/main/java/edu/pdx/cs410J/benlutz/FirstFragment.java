507d443e5c475d4de84033b2c992c21ff97c21af
benjaminlutz <benlutz@pdx.edu>
2024-03-16 22:08:24 -0700

    Getting my basic buttons set up

--- apptbook-android/app/src/main/java/edu/pdx/cs410J/benlutz/FirstFragment.java
+++ apptbook-android/app/src/main/java/edu/pdx/cs410J/benlutz/FirstFragment.java
@@ -1,20 +1,13 @@
 package edu.pdx.cs410J.benlutz;
 
-import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ScrollView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import androidx.navigation.fragment.NavHostFragment;
 
 import edu.pdx.cs410J.benlutz.databinding.FragmentFirstBinding;
 
@@ -25,49 +18,19 @@ public class FirstFragment extends Fragment {
     @Override
     public View onCreateView(
             @NonNull LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
+            Bundle savedInstanceState) {
+
         binding = FragmentFirstBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
 
+    @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.button.setOnClickListener(v -> {
-            String readmeText = readReadmeText();
-            if (!readmeText.isEmpty()) {
-                showReadmeDialog(readmeText);
-            }
-        });
-    }
-
-    private void showReadmeDialog(String readmeText) {
-        final ScrollView scrollView = new ScrollView(getContext());
-        final TextView textView = new TextView(getContext());
-        textView.setText(readmeText);
-        scrollView.addView(textView);
-
-        new AlertDialog.Builder(getContext())
-                .setTitle("README")
-                .setView(scrollView)
-                .setPositiveButton("Close", (dialog, which) -> dialog.dismiss())
-                .show();
-    }
-
-    private String readReadmeText() {
-        StringBuilder readmeText = new StringBuilder();
-        try (InputStream readmeStream = getActivity().getAssets().open("README.txt");
-             BufferedReader reader = new BufferedReader(new InputStreamReader(readmeStream))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                readmeText.append(line).append('\n');
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return readmeText.toString();
-        //return "Hello world";
+        binding.button1.setOnClickListener(v ->
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment));
     }
 
     @Override
