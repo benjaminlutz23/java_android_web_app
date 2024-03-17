7f5210ff0e4bdfed9c7a3aec86ef88d9f69779b2
benjaminlutz <benlutz@pdx.edu>
2024-03-16 17:18:10 -0700

    Commit it while it works

---
+++ apptbook-android/app/src/main/java/edu/pdx/cs410J/benlutz/AppointmentBook.java
@@ -0,0 +1,67 @@
+package edu.pdx.cs410J.benlutz;
+
+import edu.pdx.cs410J.AbstractAppointmentBook;
+
+import java.util.*;
+
+/**
+ * Represents an appointment book that holds a collection of {@link Appointment} objects.
+ * This class extends {@link AbstractAppointmentBook} and provides functionality
+ * for managing appointments for a specific owner
+ */
+public class AppointmentBook extends AbstractAppointmentBook<Appointment> {
+  /**
+   * The name of the owner of the appointment book.
+   */
+  private final String owner;
+
+  /**
+   * A sorted set of appointments in the appointment book.
+   */
+  private final SortedSet<Appointment> appointments;
+
+  /**
+   * Constructs a new AppointmentBook with the specified owner's name
+   *
+   * @param owner The name of the owner of the appointment book
+   * @throws invalidOwnerException if the owner name is empty
+   */
+  public AppointmentBook(String owner) throws invalidOwnerException {
+    if (Objects.equals(owner, "")) {
+      throw new invalidOwnerException();
+    }
+
+    this.owner = owner;
+    this.appointments = new TreeSet<>();
+  }
+
+  /**
+   * Retrieves the name of the owner of this appointment book
+   *
+   * @return The name of the owner
+   */
+  @Override
+  public String getOwnerName() {
+    return this.owner;
+  }
+
+  /**
+   * Retrieves a collection of all appointments in this appointment book
+   *
+   * @return A collection of {@link Appointment} objects
+   */
+  @Override
+  public Collection<Appointment> getAppointments() {
+    return this.appointments;
+  }
+
+  /**
+   * Adds an appointment to the appointment book
+   *
+   * @param appt The {@link Appointment} to be added to the book
+   */
+  @Override
+  public void addAppointment(Appointment appt) {
+    this.appointments.add(appt);
+  }
+}
